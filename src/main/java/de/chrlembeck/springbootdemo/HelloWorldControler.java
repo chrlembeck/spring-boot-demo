3e58bbd34c4f8335f82fbd65f20894e72425ea38
Christoph Lembeck <chrlembeck@gmail.com>
2018-05-01 20:19:22 +0200

    added new tests and formatted source code

--- src/main/java/de/chrlembeck/springbootdemo/HelloWorldControler.java
+++ src/main/java/de/chrlembeck/springbootdemo/HelloWorldControler.java
@@ -1,5 +1,6 @@
 package de.chrlembeck.springbootdemo;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldControler {
 
-    @GetMapping("/hello")
+    @GetMapping(value = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
     public String helloWorld(@RequestParam final String name) {
         return "Hello, " + name + "\n";
+    }
+
+    @GetMapping(value = "/hellohtml", produces = MediaType.TEXT_HTML_VALUE)
+    public String helloHtml(@RequestParam final String name) {
+        return "<html><body><h1>Hello, " + name + "!</h1></body></html>";
     }
 }
--- src/test/java/de/chrlembeck/springbootdemo/HelloWorldTest.java
+++ src/test/java/de/chrlembeck/springbootdemo/HelloWorldTest.java
@@ -8,8 +8,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.ContentResultMatchers;
 
 @RunWith(SpringRunner.class) // JUnit mit Spring-Konfiguration starten
 @WebMvcTest
@@ -20,9 +22,27 @@ public class HelloWorldTest {
 
     @Test
     public void helloWorldTest() throws Exception {
+        ContentResultMatchers content = content();
         mockMvc.perform(get("/hello") // http://localhost/hello Aufrufen
                 .param("name", "World")) // Parameter name=World übergeben
         .andExpect(status().isOk()) // Status auf OK (200) prüfen
-        .andExpect(content().string("Hello, World\n")); // Inhalt prüfen
+        .andExpect(content.contentTypeCompatibleWith(MediaType.TEXT_PLAIN)) // content
+        // type
+        // text/plain
+        // prüfen
+        .andExpect(content.string("Hello, World\n")); // Inhalt prüfen
+    }
+
+    @Test
+    public void helloWorldHtmlTest() throws Exception {
+        ContentResultMatchers content = content();
+        mockMvc.perform(get("/hellohtml") // http://localhost/hello Aufrufen
+                .param("name", "xyzäöü123")) // Parameter name=World übergeben
+        .andExpect(status().isOk()) // Status auf OK (200) prüfen
+        .andExpect(content.contentTypeCompatibleWith(MediaType.TEXT_HTML)) // content
+        // type
+        // text/html
+        // prüfen
+        .andExpect(content.string("<html><body><h1>Hello, xyzäöü123!</h1></body></html>")); // Inhalt prüfen
     }
 }
